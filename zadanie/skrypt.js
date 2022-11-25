7d874c88d4ccf18d6f1aa0d76bfbf8f942223bd7
pawelek69 <97956110+pawelek69@users.noreply.github.com>
2022-11-25 09:33:24 +0100

    Add files via upload

---
+++ zadanie/skrypt.js
@@ -0,0 +1,12 @@
+function obliczanie(){
+    let liczba1=document.getElementById("Wliczba1").value;
+    Lbinarna="";
+    wynik="";
+    while(liczba1>0){
+        reszta=liczba1%2
+        wynik=wynik+reszta
+        liczba1=Math.floor(liczba1/2)
+
+    }
+    document.getElementById("wynik").value=wynik;
+}
